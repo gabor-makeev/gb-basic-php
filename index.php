5316767988b8bb246855dbb4f32d34c69275981e
Gabor Makeev <gabor.makejev@gmail.com>
2022-12-10 16:36:44 +0100

    style: remove white-spaces within type casting in index.php

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 $userName = readline("Как вас зовут?" . PHP_EOL);
 
 $task1Title = readline("Какая задача стоит перед вами сегодня?" . PHP_EOL);
-$task1TimeEstimate = (int) readline("Сколько примерно времени эта задача займет?" . PHP_EOL);
+$task1TimeEstimate = (int)readline("Сколько примерно времени эта задача займет?" . PHP_EOL);
 
 $task2Title = readline("Какая задача стоит перед вами сегодня?" . PHP_EOL);
-$task2TimeEstimate = (int) readline("Сколько примерно времени эта задача займет?" . PHP_EOL);
+$task2TimeEstimate = (int)readline("Сколько примерно времени эта задача займет?" . PHP_EOL);
 
 $task3Title = readline("Какая задача стоит перед вами сегодня?" . PHP_EOL);
-$task3TimeEstimate = (int) readline("Сколько примерно времени эта задача займет?" . PHP_EOL);
+$task3TimeEstimate = (int)readline("Сколько примерно времени эта задача займет?" . PHP_EOL);
 
 echo "$userName, сегодня у вас запланировано 3 приоритетных задачи на день:"
     . PHP_EOL . "- $task1Title " . "($task1TimeEstimate" . "ч)"
