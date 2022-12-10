dca224bb7f8cd141e5d86898886995332f571992
Gabor Makeev <gabor.makejev@gmail.com>
2022-12-10 16:56:39 +0100

    refactor: replace usage of a single echo with multiple ones in index.php

--- index.php
+++ index.php
@@ -11,9 +11,8 @@ $task2TimeEstimate = (int)readline("Сколько примерно времен
 $task3Title = readline("Какая задача стоит перед вами сегодня?" . PHP_EOL);
 $task3TimeEstimate = (int)readline("Сколько примерно времени эта задача займет?" . PHP_EOL);
 
-echo "$userName, сегодня у вас запланировано 3 приоритетных задачи на день:"
-    . PHP_EOL . "- $task1Title " . "($task1TimeEstimate" . "ч)"
-    . PHP_EOL . "- $task2Title " . "($task2TimeEstimate" . "ч)"
-    . PHP_EOL . "- $task3Title " . "($task3TimeEstimate" . "ч)"
-    . PHP_EOL . "Примерное время выполнения плана = " . ($task1TimeEstimate + $task2TimeEstimate + $task3TimeEstimate) . "ч" . PHP_EOL;
-
+echo "$userName, сегодня у вас запланировано 3 приоритетных задачи на день:" . PHP_EOL;
+echo "- $task1Title " . "($task1TimeEstimate" . "ч)" . PHP_EOL;
+echo "- $task2Title " . "($task2TimeEstimate" . "ч)" . PHP_EOL;
+echo "- $task3Title " . "($task3TimeEstimate" . "ч)" . PHP_EOL;
+echo "Примерное время выполнения плана = " . ($task1TimeEstimate + $task2TimeEstimate + $task3TimeEstimate) . "ч" . PHP_EOL;
