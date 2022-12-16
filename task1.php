8a3a973b0be4338d382b66b0eab6ad8d1bad5acc
Gabor Makeev <gabor.makejev@gmail.com>
2022-12-16 23:28:48 +0100

    fix: in task1.php change the anonymous function used in the array_map function to 'return' the result and not 'echo' it

--- task1.php
+++ task1.php
@@ -3,11 +3,17 @@
 $arr = range(0, 10);
 
 array_map(function ($el) {
-    echo ($el & 1 ? "нечетное" : "четное") . PHP_EOL;
+    return ($el & 1 ? "нечетное" : "четное") . PHP_EOL;
 }, $arr);
+
+// Если бы мы хотели выводить результат:
+//
+// array_map(function ($el) {
+//     echo ($el & 1 ? "нечетное" : "четное") . PHP_EOL;
+// }, $arr);
 
 // Альтернативное решение
 //
 // array_map(function ($el) {
-//     echo ($el % 2 === 0 ? "четное" : "нечетное") . PHP_EOL;
+//     return ($el % 2 === 0 ? "четное" : "нечетное") . PHP_EOL;
 // }, $arr);
