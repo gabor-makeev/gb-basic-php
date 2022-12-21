e949f4d00cdd6248665b876317d2223442fc9a14
Gabor Makeev <gabor.makejev@gmail.com>
2022-12-21 23:28:57 +0100

    feat: task 1 completed

---
+++ Task.php
@@ -0,0 +1,104 @@
+<?php
+
+class Task
+{
+    private string $description;
+    private DateTime $dateCreated;
+    private DateTime $dateUpdated;
+    private ?DateTime $dateDone = null;
+    private ?int $priority = null;
+    private bool $isDone = false;
+    private User $user;
+
+    public function __construct(User $user, string $description)
+    {
+        $this->user = $user;
+        $this->description = $description;
+
+        $this->dateCreated = new DateTime();
+        $this->dateUpdated = new DateTime();
+    }
+
+    public function markAsDone()
+    {
+        if (!$this->isDone) {
+            $this->isDone = true;
+            $this->dateUpdated = new DateTime();
+            $this->dateDone = new DateTime();
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+        $this->dateUpdated = new DateTime();
+    }
+
+    /**
+     * @return int
+     */
+    public function getPriority(): ?int
+    {
+        return $this->priority;
+    }
+
+    /**
+     * @param int $priority
+     */
+    public function setPriority(int $priority): void
+    {
+        $this->priority = $priority;
+        $this->dateUpdated = new DateTime();
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getDateCreated(): DateTime
+    {
+        return $this->dateCreated;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getDateUpdated(): DateTime
+    {
+        return $this->dateUpdated;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getDateDone(): ?DateTime
+    {
+        return $this->dateDone;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDone(): bool
+    {
+        return $this->isDone;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+}
