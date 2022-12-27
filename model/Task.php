6ca466c4cd2936477ba8dc387c7649829a6ae018
Gabor Makeev <gabor.makejev@gmail.com>
2022-12-27 20:51:34 +0100

    feat: task 2. create Task class

---
+++ model/Task.php
@@ -0,0 +1,44 @@
+<?php
+
+class Task
+{
+    private string $description;
+    private bool $isDone = false;
+
+    public function __construct(string $description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDone(): bool
+    {
+        return $this->isDone;
+    }
+
+    /**
+     * @param bool $isDone
+     */
+    public function setIsDone(bool $isDone): void
+    {
+        $this->isDone = $isDone;
+    }
+}
