07c0e665ea22ea2d197ff4305666a057232aa159
Gabor Makeev <gabor.makejev@gmail.com>
2022-12-31 00:09:02 +0100

    feat: task 3 completed

--- routes.php
+++ routes.php
@@ -4,4 +4,5 @@ return [
     'index' => 'controller/IndexController.php',
     'security' => 'controller/SecurityController.php',
     'tasks' => 'controller/TasksController.php',
+    'signup' => 'controller/SignupController.php'
 ];
